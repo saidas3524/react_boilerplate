0cb5c9165bbb76b026f99c93103e8b00dd1e848f
Sai Krishna Dasoju <saidaso@microsoft.com>
2017-10-24 09:58:17 +0530

    temp changes removed

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Temp from './Temp';
+
 import LocaleHOC from './LocaleHOC';
 
 
@@ -17,14 +17,12 @@ class Footer extends React.Component {
         locale:PropTypes.object
     }
 
-    shouldComponentUpdate(){
-        return false;
-    }
+    
     render() {
         const {locale}  = this.props;
         return (
             <div className="footer">
-                <Temp/>
+                
                 <h1>{locale.strings.FooterName}</h1>
                 <p>{locale.strings.Footerpara}</p>
             </div>
